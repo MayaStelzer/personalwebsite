2b9db9be361fa1e925c035173eb5a874e48a400f
MayaStelzer <maya@bipper.net>
2023-11-06 14:58:50 -0800

    working on about page

---
+++ src/components/AboutSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './AboutSection.css';
+
+
+function AboutSection() {
+    return (
+        <div>
+            <figure className='profile__pic'>
+                <img src='/images/profile.jpg'/>
+            </figure>
+        </div>
+
+    )
+}
+
+export default AboutSection
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../../App.css'
 import Footer from '../Footer'
+import AboutSection from '../AboutSection'
 
 function About () {
     return (
         <>
+         <AboutSection/>
          <Footer/>
         </>
     )
