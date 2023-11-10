fb2ddd8e4092bf7c64c456e7f29ff17f241234a0
MayaStelzer <maya@bipper.net>
2023-11-10 13:19:43 -0800

    added profile images and fixed hero image

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,17 +1,27 @@
-import React, {Component} from 'react'
+import {React, useRef} from 'react'
 import './AboutSection.css';
-import {PictureData} from './PictureData';
-import {shuffle} from './helpers/shuffle';
+import profile1 from "../images/profile.jpg"
+import profile2 from "../images/profile2.jpg"
+import profile3 from "../images/profile3.jpg"
+import profile4 from "../images/profile4.jpg"
+import profile5 from "../images/profile5.jpg"
+import profile6 from "../images/profile6.jpg"
 
 function AboutSection() {
+    let imageContainerRef = useRef(null)
+    const IMAGES = [profile1, profile2, profile3, profile4, profile5, profile6]
+    const prev = () => imageContainerRef.current.scrollLeft -= 250
+    const next = () => imageContainerRef.current.scrollLeft += 250;
+
     return (
         <div className='profile'>
-            <figure className='profile__pic'>
-                <img 
-                    src='images/profile.JPG'
-                    alt='Profile Picture'
-                />
-            </figure>
+            <div className="profile__pics">
+                <div className="prev" onClick={prev}><i class="fas fa-angle-left"/></div>
+                <div className="slide-panel" ref={imageContainerRef}>
+                    {IMAGES.map(image => {return (<img src={image}/>)})}
+                </div>
+                <div className="next" onClick={next}><i class="fas fa-angle-right"/></div>
+            </div>
             <div className='profile__content'>
                 <p>My name is Maya Stelzer</p>
             </div>
--- src/components/PictureData.js
+++
@@ -1,9 +0,0 @@
-export const PictureData = [{
-    url: '/images/profile2.jpg',
-    name: 'profilepic2'
-},
-{
-    url: '/images/profile.jpg',
-    name: 'profilepic1'
-}
-];
